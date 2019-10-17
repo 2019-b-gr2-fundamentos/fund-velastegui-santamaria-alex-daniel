8f5281296db2b859a07325e6bc61a9499a7783b3
Daniel Velastegui <alex.velastegui@epn.edu.ec>
2019-10-17 17:52:32 -0500

    Clase de variables

---
+++ Lenguajes/01-variables.js
@@ -0,0 +1,28 @@
+/*
+Nombre: Daniel Velastegui
+Fecha de actualizacion: 71-10-2019
+Descripcion: Ejemplos de variables primitivas en JS
+*/
+
+//Comentario
+
+// Primitivas (TIPOS DE VARIABLES)
+/*
+int         Numero entero       Ej: 5 6 0
+char        Caracter            Ej: a b c
+float       Numeros decimales   Ej: 3.4 -2.7  
+double      N. decimales ++     Ej: 12121212...
+strings     Arreglo de char     Ej: "asdas"
+null        VACIO               Ej: null
+boolean     V/F
+*/
+
+var edad = 30; //YA NO SE USA NUNCA
+let edad = 30;  //MUTABLE
+const edad = 30; // INMUTABLE
+//edad = 30 PYTHON
+// var edad: Integer = 30 KOTLIN Mutable
+// val edad: Integer = 30 KOTLIN Inmutable
+const IVA = 14;
+let sueldo = 0;
+IVA = 12; // NO SE PUEDO REASIGNAR UN VALOR A ESTA VARIABLE
---
+++ Lenguajes/02-variables-ts.ts
@@ -0,0 +1,8 @@
+
+var edad = 30; 
+let edad = 30;  
+const edad = 30; 
+
+const IVA = 14;
+let sueldo = 0;
+IVA = 12; 
