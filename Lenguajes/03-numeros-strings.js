ef0cbf529c68a48dbc32811b1e04b30c2634fd3b
danielvelastegui <55715214+danielvelastegui@users.noreply.github.com>
2019-10-23 17:53:41 -0500

    Operaciones

---
+++ Lenguajes/03-numeros-strings.js
@@ -0,0 +1,20 @@
+var numeroUno = 1; // camelCase
+var numeroDos = 2;
+var resultado = 0;
+//SUMA  
+resultado = 1 + 2;
+//RESTAR  
+resultado = 1 - 2;
+//MULTIPLICAR  
+resultado = 1 * 2;
+//DIVIDIR  
+resultado = 1 / 2;
+//EXPONENCIAL  
+resultado = 1 ^ 2;
+//MOD  
+resultado = 4 % 2; // -> RESIDUO
+console.log("pera" + 2);
+//NUMEROUNO     -> TODO MAYUSCULA (SEPARACION NO)
+//NUMERO_UNO    -> mayusculas guion bajo
+//NumeroUno     -> primera letra de cada palabra con mayusculas
+//numerouno
