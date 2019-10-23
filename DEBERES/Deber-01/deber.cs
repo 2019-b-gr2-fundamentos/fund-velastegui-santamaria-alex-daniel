e46bb293fb7b9c03b513dd160e2d6ac9180db644
Daniel Velastegui <55715214+danielvelastegui@users.noreply.github.com>
2019-10-22 21:23:06 -0500

    Hola Mundo en C#

---
+++ DEBERES/Deber-01/deber.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Deber_01
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+        }
+    }
+}
