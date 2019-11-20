0ae2312a4ef25a395ff571b030db80ea553c12d1
Atari777 <ariel.pillajo@epn.edu.ec>
2019-11-20 17:50:05 -0500

    CLASES

--- 02-Iterables/floron1.0.js
+++ 02-Iterables/floron1.0.js
@@ -3,13 +3,13 @@ const posicion = prompt("Ingrese un numero: ")
 let condicion = 0
 const arreglo = []
 //Lenar el arreglo y comprobar que existe al menos un uno
-while(condicion == 0){
+while(condicion == false){
     for(let i = 0; i < tamaño; i++){
         arreglo[i] = Math.floor(Math.random() * 2)
     }
     for(let j = 0; j < tamaño; j++){
         if(arreglo[j] == 1){
-            condicion++
+            condicion = true
         }
     }
 }
