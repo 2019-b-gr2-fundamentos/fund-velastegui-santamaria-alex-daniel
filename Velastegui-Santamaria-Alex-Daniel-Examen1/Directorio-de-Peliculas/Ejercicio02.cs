2e9d82bf34211a234a6462a5953976b8712a9c90
danielvelastegui <55715214+danielvelastegui@users.noreply.github.com>
2019-12-06 15:58:57 -0500

    Examen 01

---
+++ Velastegui-Santamaria-Alex-Daniel-Examen1/Directorio-de-Peliculas/Ejercicio02.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Directorio_de_Peliculas
+{
+    class Ejercicio02
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
