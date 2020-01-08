f885232a09f60050263d65f94f6e704b38297fdd
Daniel Velastegui <alex.velastegui@epn.edu.ec>
2020-01-08 17:33:38 -0500

    CLASES

---
+++ 05-proyecto/index.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var funcionSuma = require("f-daniel-velastegui/lib/suma");
+var respuestaSuma = funcionSuma(2, 3);
+console.log("la respuesta de la suma es: ", respuestaSuma);
---
+++ 05-proyecto/index.ts
@@ -0,0 +1,5 @@
+import * as funcionSuma from 'f-daniel-velastegui/lib/suma'
+
+const respuestaSuma = funcionSuma(2, 3);
+
+console.log("la respuesta de la suma es: ", respuestaSuma);
