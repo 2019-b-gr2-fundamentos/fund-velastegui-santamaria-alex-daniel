ca3ccaabd89cd5d53fe64b16a86b278e04836028
aaaaaa <aaaaaa>
2020-01-09 16:38:21 -0400

    CLASES

--- 05-proyecto/index.js
+++ 05-proyecto/index.js
@@ -1,5 +1,3 @@
-"use strict";
-exports.__esModule = true;
-var funcionSuma = require("f-daniel-velastegui/lib/suma");
+var funcionSuma = require('f-daniel-velastegui/lib/suma');
 var respuestaSuma = funcionSuma(2, 3);
 console.log("la respuesta de la suma es: ", respuestaSuma);
