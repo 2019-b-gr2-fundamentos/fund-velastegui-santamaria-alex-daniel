dbdd7fe3e8893531f3f0980a870adeff77467403
Daniel Velastegui <alex.velastegui@epn.edu.ec>
2020-01-09 17:32:15 -0400

    BORRAR

--- 05-proyecto/index.ts
+++ 05-proyecto/index.ts
@@ -3,3 +3,13 @@ import * as funcionSuma from 'f-daniel-velastegui/lib/suma'
 const respuestaSuma = funcionSuma(2, 3);
 
 console.log("la respuesta de la suma es: ", respuestaSuma);
+
+import * as funcionAreaCirculo from 'f-daniel-velastegui/lib/area-circulo'
+
+const respuestaAC = funcionAreaCirculo(2);
+
+console.log("El area del circulo es: ", respuestaAC);
+
+import * as PI from 'f-daniel-velastegui/lib/pi'
+
+console.log("El numero pi es: ", PI)
